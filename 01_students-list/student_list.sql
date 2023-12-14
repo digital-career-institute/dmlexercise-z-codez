005d848ae25b18de0c3fac826a71c2ff0d66fbdf
z-codez <137389454+z-codez@users.noreply.github.com>
2023-12-14 12:06:46 +0100

    Add files via upload

---
+++ 01_students-list/student_list.sql
@@ -0,0 +1,30 @@
+create database student_list;
+
+use student_list;
+
+create table Students(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50),
+age INT,
+grade FLOAT,
+address TEXT
+);
+
+desc Students;
+
+alter table Students auto_increment=101;
+
+insert into Students(name, age, grade, address) values
+('John Doe', 20, 85.5, '123 Main St'),
+('Jane Smith', 22, 92.0, '456 Oak Ave'),
+('Bob Johnson', 21, 78.5, '789 Pine Rd'),
+('Tina Turner', 25, 71.0, '45 Columbia St');
+
+update table Students set grade=95.0 where name='Jane Smith';
+
+delete from Students where id=103;
+
+select name, age from Students;
+
+select name, grade from Students where grade>80;  
+
